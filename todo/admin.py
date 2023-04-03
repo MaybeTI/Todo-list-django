e2588e44ffe7723339a9cd7c5f8fc061ae418621
bohdanlysuchenko <lysachenko111@ukr.net>
2023-04-03 11:23:40 +0300

    Registered admin models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from todo.models import Task, Tag
 
-# Register your models here.
+
+admin.site.unregister(Group)
+admin.site.register(Task)
+admin.site.register(Tag)
