181d4a8da2bf09a3cad074cbace1c7786b54a9c7
bohdanlysuchenko <lysachenko111@ukr.net>
2023-04-03 13:19:06 +0300

    Added Create Update and Delete for Task

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from todo.views import TaskListView
+from todo.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView
 
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="home")
+    path("", TaskListView.as_view(), name="home"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
+    path("<int:pk>/toggle/", TaskListView.as_view(), name="toggle-task"),
+    path("tags/", TagListView.as_view(), name="tag-list")
+
 ]
 
 
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.views import generic
-from todo.models import Task
+from django.urls import reverse_lazy
+from todo.forms import TaskForm, TaskFormToggle
+from todo.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.all().prefetch_related("tags")
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        form = TaskFormToggle(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            if task.is_done:
+                task.is_done = False
+            else:
+                task.is_done = True
+            task.save()
+        return redirect("todo:home")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:home")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:home")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:home")
+
+
+class TagListView(generic.ListView):
+    model = Tag
